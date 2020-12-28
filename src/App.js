eef91ac6404976aab83f8e0e10a149abbb6cc15a
aliceblack <blackfluid44@gmail.com>
2020-12-28 11:52:56 +0100

    Historic events

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
+import React from 'react';
+
+
 import logo from './sword.png';
 import './App.css';
 
+
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+
+
+class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        isFetching: false,
+        items: [{"id":0}]
+    };
+  }
+
+  render() {
+    var listItems = this.state.items.map(function(item) {
+      return (
+        <VerticalTimelineElement
+          className="vertical-timeline-element"
+          contentStyle={{ background: 'white', color: 'black' }}
+          contentArrowStyle={{ borderRight: '7px solid  white' }}
+          date={item?.date}
+          iconStyle={{ background: '#282c34', color: '#fff' }}
+        >
+        <h3 className="vertical-timeline-element-title">{item?.title}</h3>
+          <h4 className="vertical-timeline-element-subtitle">{item?.subtitle}</h4>
+          <p>{item?.description}</p>
+          <img class="img-historic" src={item?.media} ></img>
+        </VerticalTimelineElement>
+      ); 
+    }); 
+    return (
+      <div> 
+        <VerticalTimeline>
+          {listItems} 
+        </VerticalTimeline>
+      </div> 
+    ); 
+  } 
+
+  componentDidMount() {
+    const current = this;
+    fetch("/items.json")
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data);
+      current.setState({items: data});
+    })
+  }
+
+
+}
 
 function App() {
   return (
@@ -11,91 +65,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <div className="App-content">
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          contentStyle={{ background: 'white', color: 'black' }}
-          contentArrowStyle={{ borderRight: '7px solid  white' }}
-          date="3100 BC"
-          iconStyle={{ background: '#282c34', color: '#fff' }}
-        >
-        <h3 className="vertical-timeline-element-title">Ancient Egypt</h3>
-          <h4 className="vertical-timeline-element-subtitle">3100 BC</h4>
-          <p>
-          Ancient Egyptian civilization followed prehistoric Egypt and coalesced around 3100 BC (according to conventional Egyptian chronology) with the political unification of Upper and Lower Egypt under Menes (often identified with Narmer).[2] The history of ancient Egypt occurred as a series of stable kingdoms, separated by periods of relative instability known as Intermediate Periods: the Old Kingdom of the Early Bronze Age, the Middle Kingdom of the Middle Bronze Age and the New Kingdom of the Late Bronze Age.
-          </p>
-          <img class="img-historic" src="https://www.history.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTYzOTQ2MTc0Njc4NzcxNTM3/egypt-gettyimages-1085205362.jpg" ></img>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          date="2010 - 2011"
-          iconStyle={{ background: '#282c34', color: '#fff' }}
-        >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          date="2008 - 2010"
-          iconStyle={{ background: '#282c34', color: '#fff' }}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-          <p>
-            User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          date="2006 - 2008"
-          iconStyle={{ background: '#282c34', color: '#fff' }}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-          <p>
-            User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          date="April 2013"
-          iconStyle={{ background: '#282c34', color: '#fff' }}
-        >
-          <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>
-            Strategy, Social Media
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          date="November 2012"
-          iconStyle={{ background: '#282c34', color: '#fff' }}
-        >
-          <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          date="2002 - 2006"
-          iconStyle={{ background: '#282c34', color: '#fff' }}
-        >
-          <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-          <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-          <p>
-            Creative Direction, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          iconStyle={{ background: '#282c34', color: '#fff' }}
-        />
-        </VerticalTimeline>
+        <Board>
+        </Board>
       </div>
     </div>
   );
